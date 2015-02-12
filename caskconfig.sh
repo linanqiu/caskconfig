d54afcd4f4adbceb55f9e1daf2a6bba79ac6c91b
Linan Qiu <linanqiu@gmail.com>
2015-02-11 21:32:09 -0500

    first

---
+++ caskconfig.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+#
+# Apps
+brew cask install dropbox
+brew cask install utorrent
+brew cask install virtualbox
+brew cask install google-hangouts
+
+# Email
+brew cask install gpgtools
+brew cask install mailmate
+
+# Video
+brew cask install vlc
+brew cask install subtitles
+brew cask install handbrake
+
+# Audio
+brew cask install xld
+brew cask install spotify
+
+# Menubar Tools
+brew cask install gfxcardstatus
+
+# IM Clients
+brew cask install skype
+brew cask install wechat
+
+# Browsers
+brew cask install google-chrome
+brew cask install torbrowser
+
+# Dev Tools
+brew cask install iterm2
+brew cask install jewelrybox
+brew cask install postgres
+brew cask install sublime-text
+brew cask install eclipse ide
+brew cask install node
+brew cask install macvim
+brew cask install java
+brew cask install python python3
+brew cask install r
+brew cask install heroku-toolbelt
+brew cask install mactex
+brew cask install latexian
+brew cask install latexit
+
+# System Utilities
+brew cask install launchrocket
+brew cask install onyx
+brew cask install the-unarchiver
+brew cask install spectacle
+brew cask install cleanmymac
+brew cask install smcfancontrol
+brew cask install bittorrent-sync
+
+# System Plugins
+brew cask install flash
+brew cask install silverlight
